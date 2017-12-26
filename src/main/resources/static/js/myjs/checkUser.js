5bc85b76f28bcaff122bfb44ceb19d2db9bfb88e
DESKTOP-4QNAVNC\Aron <jianengz2058@foxmail.com>
2017-12-26 16:18:24 +0800

    CheckUser的同意和拒绝修改相对路径修改

--- src/main/resources/static/js/myjs/checkUser.js
+++ src/main/resources/static/js/myjs/checkUser.js
@@ -9,7 +9,7 @@ function checkUserAgree() {
     var userEmailTemp = document.getElementById("userEmail").innerHTML;
     $.ajax({
         type: "post",
-        url: "/user/checkUserAgree/" + userEmailTemp,
+        url: "../user/checkUserAgree/" + userEmailTemp,
         dataType: "json",
         success: function () {
             console.log("11111111111111111111111111111111111111111");
@@ -22,7 +22,7 @@ function checkUserReject() {
     var userEmailTemp = document.getElementById("userEmail").innerHTML;
     $.ajax({
         type: "post",
-        url: "/user/checkUserReject/" + userEmailTemp,
+        url: "../user/checkUserReject/" + userEmailTemp,
         dataType: "json",
         success: function () {
             console.log("2222222222222222222");
