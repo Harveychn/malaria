a8c17a716a9d97715cf9c168144d59dfa278c2f3
R git config --global user.name R <958844609@qq.com>
2018-01-09 18:03:08 +0800

    下载数据选取字段顺序调整

--- src/main/java/com/edupractice/malaria/modules/download/pojo/CategoryFieldsRe.java
+++ src/main/java/com/edupractice/malaria/modules/download/pojo/CategoryFieldsRe.java
@@ -1,8 +1,17 @@
 package com.edupractice.malaria.modules.download.pojo;
 
 public class CategoryFieldsRe {
+    private int indicatorId;
     private String fieldName;
     private String displayName;
+
+    public int getIndicatorId() {
+        return indicatorId;
+    }
+
+    public void setIndicatorId(int indicatorId) {
+        this.indicatorId = indicatorId;
+    }
 
     public String getFieldName() {
         return fieldName;
