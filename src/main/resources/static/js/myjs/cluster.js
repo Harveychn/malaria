1c879e96d9011f26e1925cb4013f3a0c1ed2d02d
RsJourna1 <958844609@qq.com>
2018-05-29 14:51:06 +0800

    聚类分析添加点击省份显示详细数据

--- src/main/resources/static/js/myjs/cluster.js
+++ src/main/resources/static/js/myjs/cluster.js
@@ -21,11 +21,9 @@ $('#submitTerm').on('click', function (event) {
     var attribute = $('#attribute option:selected').val();
     var diseaseName = $('#diseaseName option:selected').val();
 
-    //创建Map实例
+
     var map = new BMap.Map("allMap");
-    //设置地图中心点和缩放级别
     map.centerAndZoom(new BMap.Point(116.403765, 39.914850), 5);
-    //启动鼠标滚轮操作
     map.enableScrollWheelZoom();
 
     $(function () {
@@ -41,10 +39,10 @@ $('#submitTerm').on('click', function (event) {
             success:
                 function (data) {
                     for (var i = 0; i < data.length; i++) {
-                        var color = ["#8EE5EE", "#CDAD00", "#FF7F00","#BF3EFF"];
+                        var color = ["#8EE5EE", "#CDAD00", "#FF7F00", "#BF3EFF"];
                         console.log(data[i]);
                         for (var j = 0; j < data[i].length; j++) {
-                            getBoundary(data[i][j].province, color[i]);
+                            getBoundary(data[i][j], color[i]);
                         }
                     }
                 },
@@ -54,10 +52,9 @@ $('#submitTerm').on('click', function (event) {
         });
     });
 
-
-    function getBoundary(province, color) {
+    function getBoundary(data, color) {
         var bdary = new BMap.Boundary();
-        bdary.get(province, function (rs) {       //获取行政区域
+        bdary.get(data.province, function (rs) {       //获取行政区域
             var count = rs.boundaries.length; //行政区域的点有多少个
             if (count === 0) {
                 // alert('未能获取当前输入行政区域');
@@ -68,9 +65,26 @@ $('#submitTerm').on('click', function (event) {
                     strokeWeight: 2,
                     strokeColor: "#ff9949",
                     fillColor: color
-                }); //建立多边形覆盖物
+                });//建立多边形覆盖物
+                ply.addEventListener("click",function () {
+                    var text=data.province+' \n ';
+                    var temp=0;
+                    for(var key in data.patientNum){
+                        text+= key + ":" + data.patientNum[key]+" ";
+                        temp++;
+                        if(temp%3==0){
+                            text+=' \n ';
+                        }
+                    }
+                    document.getElementById("careerDisplay").innerText = text;
+                });
+                // ply.addEventListener("mouseout",function () {
+                //     var text="";
+                //     document.getElementById("careerDisplay").innerText = text;
+                // });
                 map.addOverlay(ply);  //添加覆盖物
             }
         });
-    };
+
+    }
 });
